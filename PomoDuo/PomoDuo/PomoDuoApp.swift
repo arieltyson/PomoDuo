b62baabcf696f889b364e8a3274f80dde4889adc
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2026-02-14 13:20:43 -0800

    initial commit

---
+++ PomoDuo/PomoDuo/PomoDuoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoDuoApp.swift
+//  PomoDuo
+//
+//  Created by Ariel Tyson on 14/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PomoDuoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PomoDuo/PomoDuoTests/PomoDuoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PomoDuoTests.swift
+//  PomoDuoTests
+//
+//  Created by Ariel Tyson on 14/2/26.
+//
+
+import Testing
+@testable import PomoDuo
+
+struct PomoDuoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
